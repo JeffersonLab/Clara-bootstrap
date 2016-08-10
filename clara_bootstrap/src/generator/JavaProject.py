0540afe3c00bc0ee296cbe4f008061a719fa4e42
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-10 17:36:04 -0400

    Creates java service folders

--- clara_bootstrap/src/generator/JavaProject.py
+++ clara_bootstrap/src/generator/JavaProject.py
@@ -1,10 +1,18 @@
 # coding=utf-8
 
+from clara_bootstrap.src.utils.SystemUtils import create_folder
 from clara_bootstrap.src.res.Extras import LICENSE, GITIGNORE_JAVA, README
 
+GRADLE_SRC_FOLDER = "/src/main/java"
+GRADLE_RES_FOLDER = "/src/main/res"
+GRADLE_TEST_FOLDER = "/src/test/java"
 
 def create_java_service(project_name):
-    pass
+    # Create project folders
+    create_folder(project_name + GRADLE_SRC_FOLDER)
+    create_folder(project_name + GRADLE_RES_FOLDER)
+    create_folder(project_name + GRADLE_TEST_FOLDER)
+
 
 def create_root_dir_extras(project_name):
     # README.md
@@ -19,3 +27,16 @@ def create_root_dir_extras(project_name):
     p_gitignore = open(project_name + "/.gitignore", "w")
     p_gitignore.write(GITIGNORE_JAVA)
     p_gitignore.close()
+
+
+def create_java_project(project_name):
+
+    if project_name:
+        print "Creating the CLARA project skeleton..."
+        create_java_service(project_name)
+        # create_java_setup(project_name)
+        create_root_dir_extras(project_name)
+        print "Project created."
+
+    else:
+        raise Exception("Project name is required for scaffolding...")

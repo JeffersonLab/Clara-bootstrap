5611f0260b5a32dad42d375ca182ccd5d3506e05
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-10 15:38:06 -0400

    Add entire python scaffold

--- tests/test_pythonProjectGenerator.py
+++ tests/test_pythonProjectGenerator.py
@@ -1,23 +1,35 @@
 # coding=utf-8
 
 import os
+import shutil
 import unittest
 
 from clara_bootstrap.src.generator.PythonProjectGenerator import *
 
+project_name = "TEST_PROJECT"
+package_dir = project_name + "/" + project_name + "/"
+
 
 class TestPythonProjectGenerator(unittest.TestCase):
 
+    def tearDown(self):
+        shutil.rmtree(project_name)
+
     def test_create_python_service(self):
-        project_name = "TEST_PROJECT"
         create_python_service(project_name)
         self.assertTrue(os.path.exists(project_name))
-        self.assertTrue(os.path.isfile(project_name + "/"
-                                       + project_name + "/"
+        self.assertTrue(os.path.isfile(package_dir
+                                       + "/__init__.py"))
+        self.assertTrue(os.path.isfile(package_dir
                                        + "/TEST_PROJECTService.py"))
-        os.remove(project_name + "/"
-                  + project_name + "/"
-                  + "/TEST_PROJECTService.py")
+
+    def test_create_python_project(self):
+        create_python_project(project_name)
+        self.assertTrue(os.path.isfile(project_name + "/README.md"))
+        self.assertTrue(os.path.isfile(project_name + "/LICENSE"))
+        self.assertTrue(os.path.isfile(project_name + "/.gitignore"))
+        self.assertTrue(os.path.isfile(project_name + "/setup.py"))
+
 
 if __name__ == "__main__":
     unittest.main()

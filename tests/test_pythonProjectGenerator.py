09adabe1ef44bed5a6825e002774539dff2ba77a
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-11 18:18:51 -0400

    Update tests

--- tests/test_pythonProjectGenerator.py
+++ tests/test_pythonProjectGenerator.py
@@ -7,7 +7,8 @@ import unittest
 from clara_bootstrap.src.generator.PythonProject import *
 
 project_name = "TEST_PROJECT"
-package_dir = project_name + "/" + project_name + "/"
+src_file_path = "TEST_PROJECT/TEST_PROJECT/"
+test_file_path = "TEST_PROJECT/tests/"
 
 
 class TestPythonProjectGenerator(unittest.TestCase):
@@ -21,8 +22,12 @@ class TestPythonProjectGenerator(unittest.TestCase):
         self.assertTrue(os.path.isfile(project_name + "/LICENSE"))
         self.assertTrue(os.path.isfile(project_name + "/.gitignore"))
         self.assertTrue(os.path.isfile(project_name + "/setup.py"))
-        self.assertTrue(os.path.isfile(project_name +
-                                       "/tests/test_TEST_PROJECTService.py"))
+        self.assertTrue(os.path.isfile(src_file_path + "__init__.py"))
+        self.assertTrue(os.path.isfile(test_file_path + "__init__.py"))
+        self.assertTrue(os.path.isfile(src_file_path +
+                                       "TEST_PROJECTService.py"))
+        self.assertTrue(os.path.isfile(test_file_path +
+                                       "test_TEST_PROJECTService.py"))
 
 
 if __name__ == "__main__":

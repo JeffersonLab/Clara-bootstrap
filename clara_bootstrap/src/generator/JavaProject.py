eda6590aa98f1271c39e89713623d4682b8590fc
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-10 21:21:27 -0400

    Java: Add java service creator

--- clara_bootstrap/src/generator/JavaProject.py
+++ clara_bootstrap/src/generator/JavaProject.py
@@ -1,5 +1,7 @@
 # coding=utf-8
 
+from jinja import Environment, PackageLoader
+
 from clara_bootstrap.src.utils.SystemUtils import create_folder
 from clara_bootstrap.src.res.Extras import LICENSE, GITIGNORE_JAVA, README
 
@@ -7,11 +9,26 @@ GRADLE_SRC_FOLDER = "/src/main/java"
 GRADLE_RES_FOLDER = "/src/main/res"
 GRADLE_TEST_FOLDER = "/src/test/java"
 
+env = Environment(loader=PackageLoader("clara_bootstrap",
+                                       "src/res/templates"))
+
+
 def create_java_service(project_name):
+    package_name = project_name + GRADLE_SRC_FOLDER + "/services/"
+    test_folder = project_name + GRADLE_TEST_FOLDER
+    res_folder = project_name + GRADLE_RES_FOLDER
+    service_name = package_name + project_name + "Service.java"
+
     # Create project folders
-    create_folder(project_name + GRADLE_SRC_FOLDER)
-    create_folder(project_name + GRADLE_RES_FOLDER)
-    create_folder(project_name + GRADLE_TEST_FOLDER)
+    create_folder(package_name)
+    create_folder(test_folder)
+    create_folder(res_folder)
+
+    # Create the service
+    service_file = open(service_name, "w")
+    service_template = env.get_template("services/java.txt")
+    service_file.write(service_template.render(project_name=project_name))
+    service_file.close()
 
 
 def create_root_dir_extras(project_name):

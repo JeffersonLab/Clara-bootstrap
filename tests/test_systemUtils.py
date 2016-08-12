282e8498dc0d70a82b07aa0135610907b5f1cfd3
Ricardo Oyarzun <royarzun@gmail.com>
2016-08-12 17:18:39 -0400

    Refactor test: add setUP/tearDown methods

--- tests/test_systemUtils.py
+++ tests/test_systemUtils.py
@@ -5,18 +5,24 @@ import unittest
 
 from clara_bootstrap.src.utils.SystemUtils import *
 
+FOLDER_NAME = "TEST_FOLDER"
+
 
 class TestSystemUtils(unittest.TestCase):
+
+    def setUp(self):
+        create_folder(FOLDER_NAME)
+
+    def tearDown(self):
+        os.removedirs(FOLDER_NAME)
+        self.assertFalse(os.path.exists(FOLDER_NAME))
 
     def test_get_username(self):
         self.assertIsInstance(get_username(), basestring)
 
     def test_create_folder(self):
-        folder_name = "TEST_FOLDER"
-        create_folder(folder_name)
-        self.assertTrue(os.path.exists(folder_name))
-        os.removedirs(folder_name)
-        self.assertFalse(os.path.exists(folder_name))
+        self.assertTrue(os.path.exists(FOLDER_NAME))
+
 
 if __name__ == "__main__":
     unittest.main()
